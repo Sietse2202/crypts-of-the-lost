78098156bbc1e19628f82a2eb8dd9efe48874de5
Jonas Arter <jonasarter@outlook.com>
2025-07-21 14:30:57 +0200

    not working

---
+++ crates/config/src/cli.rs
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2025 Crypts of the Lost Team
+
+//! # `CLI`
+//! Defines the `CLI` struct used to provided the TOML config file path
+//! and to overwrite settings from it. Will be read from
+
+use std::path::PathBuf;
+
+/// CLI struct to provide the config file and overwrite settings from it.
+#[derive(Debug, clap::Parser)]
+#[command(author = "Crypts of the Lost")]
+pub struct Cli {
+    /// The path to the config file (default = `config.toml`)
+    #[arg(short, long, default_value = "config.toml")]
+    config: PathBuf, // ignored by figment
+}
--- crates/config/src/config.rs
+++ crates/config/src/config.rs
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2025 Crypts of the Lost Team
 
-//! # Config
-//! Defines the config struct used for the game server. Will be read from
-//! a TOML file and the CLI can override those settings.
+//! # `Config`
+//! Defines the `Config` struct used for the game server. Will be read from
+//! a TOML file and be used as a resource in bevy.
 
 /// The main `Config` struct used to configure the server.
-#[derive(Debug)]
-pub struct Config {}
+#[derive(Debug, Resource)]
+pub struct Config {
+    /// Maximum amount of players on the server
+    pub max_players: u32,
+}
