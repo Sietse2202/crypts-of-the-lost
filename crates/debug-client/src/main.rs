f87a99dc47d9e770ebb123fd98a6634c552f43dc
Sietse <sietsevanderhoeven@gmail.com>
2025-07-28 23:16:19 +0200

    Create debug-client crate

---
+++ crates/debug-client/src/main.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2025 Crypts of the Lost Team
+
+//! # Debug Client
+//!
+//! Debug CLI client for Crypts of the lost.
+//!
+
+fn main() {
+    println!("Hello, world!");
+}
