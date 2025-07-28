4ec396c7309f492150a1a66e48eceddf6b7cb9f6
Sietse <sietsevanderhoeven@gmail.com>
2025-07-28 23:16:19 +0200

    Create debug-client crate
    # Conflicts:
    #	Cargo.lock
    #	Cargo.toml

--- crates/debug-client/src/main.rs
+++ crates/debug-client/src/main.rs
@@ -2,9 +2,10 @@
 // Copyright (C) 2025 Crypts of the Lost Team
 
 //! # Debug Client
+//!
 //! Debug CLI client for Crypts of the lost.
+//!
 
-#[expect(clippy::print_stdout)]
 fn main() {
     println!("Hello, world!");
 }
