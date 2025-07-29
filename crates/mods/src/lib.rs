a04b882ad95548fec8c1dc4c86789db1a25f95cf
Sietse <sietsevanderhoeven@gmail.com>
2025-07-29 19:10:13 +0200

    License + structure

--- crates/mods/src/lib.rs
+++ crates/mods/src/lib.rs
@@ -1,10 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2025 Crypts of the Lost Team
+
 //! # `mods`
 //! This crate aims to add support for modding of the game. It provides this support via a bevy
 //! [`Plugin`]
 //!
 //! [`Plugin`]: Plugin
+
+mod toml;
+
 use bevy::app::App;
 use bevy::prelude::Plugin;
+
+/// The mod directory in relation to the server root folder
+pub const MOD_DIR: &str = "mods";
+
+/// The name of the toml file of each mod
+pub const MOD_CONFIG_FILE: &str = "mod.toml";
 
 /// # Modding Plugin
 /// Adds modding support to the main server bevy app.
